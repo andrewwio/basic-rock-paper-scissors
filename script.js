12d3888fe0274ad887aab6638a6857b418bb5a5f
Andrew Kain <hello@andreww.io>
2022-06-16 12:25:59 +0400

    console log randomizer working!

--- script.js
+++ script.js
@@ -11,6 +11,24 @@ changeColor = () => {
   }
 }
 
+const shoot = document.getElementById("shoot");
 
+shoot.onclick = () => {
+  computerPlay((randomizer(3)));
+}
 
+randomizer = (max) => {
+  return Math.floor(Math.random() * max);
+}
 
+computerPlay = () => {
+  if (randomizer(3) == 0) {
+    console.log("Rock");
+  } else if (randomizer(3) == 1) {
+    console.log("Paper");
+  } else if (randomizer(3) == 2) {
+    console.log("Scissors");
+  } else {
+    console.log("Error in computerPlay function");
+  }
+}
